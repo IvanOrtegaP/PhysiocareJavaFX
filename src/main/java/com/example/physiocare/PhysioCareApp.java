a731a6424bf333a1494be25629ed1a4fd1da96e9
Iván Ortega <ivan83raquel@gmail.com>
2025-06-09 20:44:58 +0200

    Añadidas partes de  fucnoinalidades de envio de correos pacientes, de las nominas y creacion de sus pdfs

--- src/main/java/com/example/physiocare/PhysioCareApp.java
+++ src/main/java/com/example/physiocare/PhysioCareApp.java
@@ -1,5 +1,6 @@
 package com.example.physiocare;
 
+import com.example.physiocare.services.EmailSchedulerService;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,6 +11,7 @@ import java.io.IOException;
 public class PhysioCareApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        EmailSchedulerService.startEmailScheduler();
         FXMLLoader fxmlLoader = new FXMLLoader(PhysioCareApp.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 720, 540);
         stage.setTitle("Physiocare");
